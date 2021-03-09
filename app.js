3c11aefe5ce4af3b7e0db71181f0a6801fb60c7b
odbayar <odbayar@interactive.mn>
2021-03-09 14:00:08 +0800

    Shoog shiddeg bolson

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ var playerScore = [0, 0];
 
 //Тоглогчийн ээлжиндээ цугуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
-//Шооны аль талаараа буусныг хадгалах хувьсагч
-var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-window.document.querySelector("#score-0").textContent = playerScore[0];
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-window.document.querySelector("#score-1").textContent = playerScore[1];
-
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+var diceDOM = window.document.querySelector(".dice");
+diceDOM.style.display = "none";
+window.document
+  .querySelector(".btn-roll")
+  .addEventListener("click", function () {
+    //Шооны аль талаараа буусныг хадгалах хувьсагч
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + diceNumber + ".png";
+  });
 
 //<img src="dice-5.png" alt="Dice" class="dice" />
-//window.document.querySelector(".dice").style.display = "none";
-
-window.document.querySelector(".dice").attributes = "dice-2.png";
-
-var log = window.document.getElementById("current-0");
-console.log(log);
